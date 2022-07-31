dd197105d717ca7f7c511b327c3dfde55613afe0
URK21CS1072 <100009296+URK21CS1072@users.noreply.github.com>
2022-07-31 23:10:10 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,11 +5,10 @@ class Stack:
         self.size = size
 
     def is_empty(self):
-        
-        return len(self.items)==0
+        return len(self.items) == 0
 
     def is_full(self):
-        return len(self.items)==size
+        return len(self.items) == size
 
     def push(self, data):
         if not self.is_full():
@@ -29,7 +28,7 @@ stack = Stack(size)
 for line in range(queries):
     values = list(map(int, input().rstrip().split()))
     if values[0] == 1:
-        stack.push(values[1]) 
+        stack.push(values[1])
     elif values[0] == 2:
         stack.pop()
 stack.status()
